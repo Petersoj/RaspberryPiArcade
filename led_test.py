f8f215487a5cc3b87be7e91835c0f327ca784d68
Jacob Peterson <net@jacobpeterson.net>
2019-11-16 16:23:35 -0700

    Square animation

--- led_test.py
+++ led_test.py
@@ -1,7 +1,22 @@
 from server.board.Board import *
-from server.leddriver.leddriver import update_led_matrix
+from server.leddriver.leddriver import *
+import RPi.GPIO as GPIO
 import time
 
-b = Board(8,8)
-update_led_matrix(b)
-time.sleep(15)
+size = 0
+try:
+    while True:
+        b = Board(8, 8)
+        for i in range(size):
+            b.board[i][i] = True
+
+        update_led_matrix(b)
+        if is_p2_up():
+            size = abs((size + 1) % 8)
+        elif is_p2_down():
+            size = (size + 7) % 8
+
+        time.sleep(0.1)
+
+except KeyboardInterrupt:  # If CTRL+C is pressed, exit cleanly:
+    GPIO.cleanup()
